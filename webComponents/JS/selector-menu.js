54c18e2ee988ffa6e108bd81e5cfece67f76312f
Ap97 <anthony66661@live.com>
2023-04-27 08:46:47 -0500

    Inicio de prueba branchs

--- webComponents/JS/selector-menu.js
+++ webComponents/JS/selector-menu.js
@@ -6,15 +6,15 @@ let rv = document.getElementById('page-RV');
 
 switch (nomPag) {
     case 'simulador.html':
-        simulator.style.backgroundColor = "#034492";
+        simulator.style.backgroundColor = "#0070C2";
         simulator.style.color = "#fff";
         break;
     case 'index.html':
-        index.style.backgroundColor = "#034492";
+        index.style.backgroundColor = "#0070C2";
         index.style.color = "#fff";
         break;
     case 'app.html':
-        rv.style.backgroundColor = "#034492";
+        rv.style.backgroundColor = "#0070C2";
         rv.style.color = "#fff";
         break;
     default: console.log(nomPag);
