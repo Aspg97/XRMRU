e245149313c95068fc432c1cac4563ab9056f854
Ap97 <anthony66661@live.com>
2023-05-03 02:17:50 -0500

    propuesta estilos simulador terminada

--- webComponents/JS/funsimulator.js
+++ webComponents/JS/funsimulator.js
@@ -1,13 +1,17 @@
 var numObj = document.getElementById("num-obj");
 const idTitulo = document.getElementById("id-title-simu");
+const backTitulo = document.querySelector(".cont-subtitle-state");
+document.querySelector(".btn-play").disabled = true;
 let crearObj = document.querySelector(".obj");
 let generar = document.getElementById("btn-gen");
 let calcular = document.getElementById("btn-simu");
 let conSimS = document.querySelector(".cont-simu-street");
 let frag = document.createDocumentFragment();
-var cont = false;
+
+
+
 generar.addEventListener("click", () => {
-    cont = true; // La variable detecta el click en el boton generar para cambiar la leyenda del titlo "Simulador en espera de objetos"
+    
     crearObj.innerHTML = "";
     conSimS.innerHTML = "";
     console.log("Se genero " + numObj.value + " elementos");
@@ -56,15 +60,16 @@ generar.addEventListener("click", () => {
         div.id = "street"
         div.innerHTML = `<label class="eti-a">Auto ` + (i + 1) + `</label><div class="cont-auto cont-auto-` + i + `"><img src="../IMG/Simulator/componente-auto-` + i + `.png"  class="auto-simu"></div><img src="../IMG/Simulator/componente-asfalto.png">`;
         frag.appendChild(div); // Agregamos las diferentes pistas con los autos segun la seleccion de elementos
-        idTitulo.textContent = "Simulación 2D";
-        idTitulo.classList.add("traslation-tittle");
     }
     crearObj.appendChild(objUni);
     conSimS.appendChild(frag);
+    idTitulo.textContent = "Llena mínimo 2 datos de cada objeto";
+    backTitulo.style.backgroundImage = 'url("../IMG/Simulator/fondo-estado-amarillo.png")';
 });
 //let conInp = document.getElementById("cont-inp-info");
 let op;
 calcular.addEventListener("click", () => {
+    let afi = true;
     console.log("Calculando");
     for (let i = 0; i < numObj.value; i++) {
         let idV = "inpV" + i;
@@ -75,6 +80,8 @@ calcular.addEventListener("click", () => {
         const inpT = document.getElementById(idT);
         if ((inpV.value == "" & inpD.value == "") || (inpV.value == "" & inpT.value == "") || (inpD.value == "" & inpT.value == "")) {
             alert("Llene almenos 2 datos de cada objeto");
+            afi = false;
+            break;
         } else if (inpV.value != "" & inpD.value != "") {
             op = "tiempo";
             inpT.value = definirOP(inpV.value, inpD.value, op, i);
@@ -88,6 +95,14 @@ calcular.addEventListener("click", () => {
             inpV.value = definirOP(inpD.value, inpT.value, op, i);
 
         }
+    }
+    if(afi == true){
+        idTitulo.textContent = "Simulación 2D: estamos ready!";
+        backTitulo.style.backgroundImage = 'url("../IMG/Simulator/fondo-estado-verde.png")';
+        document.querySelector(".btn-play").disabled = false;
+        btn_play.style.background = "#034492"
+        btn_play.style.color = "#fff"
+
     }
 });
 
