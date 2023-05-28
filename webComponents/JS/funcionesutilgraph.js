e3150cafe69de0b5a6506b05604aafa65359a2e4
Ap97 <anthony66661@live.com>
2023-05-28 03:07:14 -0500

    avance graficadora v2.5

--- webComponents/JS/funcionesutilgraph.js
+++ webComponents/JS/funcionesutilgraph.js
@@ -1,49 +1,44 @@
 //INICIO PROCESO >> inicializacion del grafico con canvas
+const canvas = document.getElementById("canvas");
+const pincel = canvas.getContext("2d");
+canvas.width = 700;
+canvas.height = 300;
 function ini() {
-    const canvas = document.getElementById("canvas");
-    const pincel = canvas.getContext("2d");
-    canvas.width = 700;
-    canvas.height = 400;
-    pincel.strokeStyle = "#000";
     pincel.lineWidth = 1;
-    // EjeX
+    // EjeY
     pincel.beginPath();
-    pincel.moveTo(25, 0);
-    pincel.lineTo(25, canvas.height);
+    pincel.moveTo(35, 0);
+    pincel.lineTo(35, canvas.height);
     pincel.stroke();
     pincel.closePath();
-    // EjeX > direccion+
-    pincel.fillStyle = "#000"
+    // EjeY > direccion+
     pincel.beginPath();
-    pincel.moveTo(25, 0);
-    pincel.lineTo(28, 10);
-    pincel.lineTo(22, 10);
+    pincel.moveTo(35, 0);
+    pincel.lineTo(38, 10);
+    pincel.lineTo(32, 10);
     pincel.fill();
     pincel.closePath();
-    // EjeX > direccion-
-    pincel.fillStyle = "#000"
+    // EjeY > direccion-
     pincel.beginPath();
-    pincel.moveTo(25, canvas.height);
-    pincel.lineTo(28, canvas.height - 10);
-    pincel.lineTo(22, canvas.height - 10);
+    pincel.moveTo(35, canvas.height);
+    pincel.lineTo(38, canvas.height - 10);
+    pincel.lineTo(32, canvas.height - 10);
     pincel.fill();
     pincel.closePath();
-    // EjeY
+    // EjeX
     pincel.beginPath();
     pincel.moveTo(0, canvas.height - 25);
     pincel.lineTo(canvas.width, canvas.height - 25);
     pincel.stroke();
     pincel.closePath();
-    // EjeY > direccion+
-    pincel.fillStyle = "#000"
+    // EjeX > direccion-
     pincel.beginPath();
     pincel.moveTo(0, canvas.height - 25);
     pincel.lineTo(10, canvas.height - 28);
     pincel.lineTo(10, canvas.height - 22);
     pincel.fill();
     pincel.closePath();
-    // EjeY > direccion-
-    pincel.fillStyle = "#000"
+    // EjeX > direccion+
     pincel.beginPath();
     pincel.moveTo(canvas.width, canvas.height - 25);
     pincel.lineTo(canvas.width - 10, canvas.height - 28);
@@ -52,12 +47,100 @@ function ini() {
     pincel.closePath();
     // Letra Eje Y
     pincel.font = "12px Arial";
-    pincel.fillText("Y", 10, 20);
+    pincel.fillText("d(m)", 7, 20);
     // Letra Eje X
-    pincel.font = "12px Arial";
-    pincel.fillText("X", canvas.width - 20, canvas.height - 7);
+    pincel.fillText("t(s)", canvas.width - 20, canvas.height - 7);
     // Valor de origen
-    pincel.font = "12px Arial";
-    pincel.fillText("0", 10, canvas.height - 7);
+    pincel.fillText("0", 20, canvas.height - 7);
 }
 //FIN PROCESO >> inicializacion del grafico con canvas
+//INICIO PROCESO >> dibujar numeros
+//Dibujar numeros > EjeY
+const linNumY = ({ porcentaje = 0, etinum = 1 }) => ({
+    x1: 30,
+    y1: (((canvas.height - 80) / 100) * porcentaje) + (canvas.height - 25),
+    x2: 40,
+    y2: (((canvas.height - 80) / 100) * porcentaje) + (canvas.height - 25),
+    etinum,
+    fontSS: "12px Arial",
+    dibujarNumero() {
+        pincel.beginPath();
+        pincel.moveTo(this.x1, this.y1);
+        pincel.lineTo(this.x2, this.y2);
+        pincel.stroke();
+        pincel.closePath();
+        pincel.font = this.fontSS;
+        pincel.fillText(etinum, this.x1 - 25, this.y1 + 4);
+    }
+});
+//Dibujar numeros > EjeX
+const linNumX = ({ porcentaje = 20, etinum = 1 }) => ({
+    x1: (((canvas.width - 80) / 100) * porcentaje) + 35,
+    y1: canvas.height - 30,
+    x2: (((canvas.width - 80) / 100) * porcentaje) + 35,
+    y2: canvas.height - 20,
+    etinum,
+    fontSS: "12px Arial",
+    dibujarNumero() {
+        pincel.beginPath();
+        pincel.moveTo(this.x1, this.y1);
+        pincel.lineTo(this.x2, this.y2);
+        pincel.stroke();
+        pincel.closePath();
+        pincel.font = this.fontSS;
+        pincel.fillText(etinum, this.x2 - 5, this.y1 + 30);
+    }
+});
+//FIN PROCESO >> dibujar numeros
+//INICIO PROCESO >> nomenclatura de punto del segmento
+const componentesSegmento = ({letra = 'a',px = 0, py = 0}) => ({
+    letra,
+    x: (((canvas.width - 80) / 100) * px) + 22,
+    y: (((canvas.height - 80) / 100) * py) + (canvas.height - 25),
+    fontSS: "15px Arial",
+    dibujarLetra() {
+        pincel.beginPath();
+        pincel.fillStyle = "red";
+        pincel.font = this.fontSS;
+        pincel.fillText(this.letra, this.x, this.y);
+        pincel.closePath();
+    },
+    dibujarPunto(){
+        pincel.beginPath();
+        pincel.fillStyle = "#034492";
+        pincel.arc(this.x+13,this.y,3,0,2*Math.PI,false);
+        pincel.fill();
+        pincel.closePath();
+    }
+});
+//FIN PROCESO >> nomenclatura de punto del segmento
+//INICIO PROCESO >> dibujar segmento
+const segmento = ({px1 = 0,py1 = 0 , px2 = 50, py2 = 50}) => ({
+    x1: (((canvas.width - 80) / 100) * px1) + 35,
+    y1: (((canvas.height - 80) / 100) * py1) + (canvas.height - 25),
+    x2: (((canvas.width - 80) / 100) * px2) + 35,
+    y2: (((canvas.height - 80) / 100) * py2) + (canvas.height - 25),
+    dibujarSegmento(){
+        pincel.beginPath();
+        pincel.strokeStyle = "#0070C2";
+        pincel.moveTo(this.x1,this.y1);
+        pincel.lineTo(this.x2,this.y2);
+        pincel.stroke();
+        pincel.closePath();
+    }
+});
+//FIN PROCESO >> dibujar segmento
+//INICIO PROCESO >> dibujar lineas guias
+const guias = ()=>({
+    limit,
+    x1,
+    y1,
+    x2,
+    y2,
+    dibujarGuia(){
+        pincel.beginPath();
+        pincel.strokeStyle = "#555";
+        pincel
+    }
+})
+//FIN PROCESO >> dibujar lineas guias
--- webComponents/JS/fungraficadora.js
+++ webComponents/JS/fungraficadora.js
@@ -5,6 +5,7 @@ const contDateGraph = document.querySelector(".cont-date-graph");
 const contInfoGraph = document.querySelector(".cont-info-graph");
 const contGraph = document.querySelector(".cont-graph");
 
+ini(); // Inicializacion de canvas
 var slcAuto = true; // Variable para seleccion de modo
 //INICIO PROCESO >> cuando el usuario selecciona personalizado
 btnPerf.addEventListener("click", () => {
@@ -39,11 +40,125 @@ function btnSelected(auto) {
     }
 }
 //FIN PROCESO >> estilo seleccion tipo de generar
-ini(); // Inicializacion de canvas
 //INICIO PROCESO >> accion del boton generar
 var numSeg = 5;
 btnGen.addEventListener("click", () => {
-    console.log("has presionado generar");
-
+    pincel.fillStyle = "#000";
+    pincel.strokeStyle = "#000";
+    pincel.clearRect(0, 0, canvas.width, canvas.height);
+    let numY = 5;
+    let numX = 8;
+    ini();
+    //Ingreso valores een recta
+    const valoresY = llenarDatos(numY); //Ingreso valores een recta > Eje Y
+    const valoresX = llenarDatos(numX);  //Ingreso valores een recta > Eje X
+    //impresion de valores
+    dibujarDatos(numY, valoresY, "Y"); //Impresion de valores > Eje Y
+    dibujarDatos(numX, valoresX, "X"); //Impresion de valores > Eje X
+    //Preparacion para graficar
+    let letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]; //Preparacion para graficar > letras para nodos de segmento
+    let cy = []; // arreglo para las coordenadas de Y
+    //Preparacion para graficar > Se llenan espacios aleatorios entre 0 y 7 con los elementos del vector de Y
+    for (let i = 0; i < numY; i++) {
+        rn = Math.floor(Math.random() * ((numX - 1) - 0 + 1)) + 0;
+        if (cy[rn] == null) {
+            cy[rn] = valoresY[0][i];
+        } else {
+            i--;
+        }
+    }
+    //Preparacion para graficar > Se llenan los espacios vacios del vector de coordenadas de y "cy"
+    for (let i = 0; i < numX; i++) {
+        if (cy[0] == null) {
+            cy[0] = valoresY[0][Math.floor(Math.random() * ((numY - 1) - 0 + 1)) + 0]
+        } else if (cy[i] == null) {
+            cy[i] = cy[i - 1];
+        }
+    }
+    // Imprimir Segmento
+    for (let i = 0; i < numX; i++) {
+        if (i == 0) {
+            const dibSegmento = segmento({
+                px1: 0,
+                py1: 0,
+                px2: valoresX[0][i] * 100 / valoresX[1],
+                py2: -cy[i] * 100 / valoresY[1]
+            });
+            dibSegmento.dibujarSegmento();
+        }else {
+            const dibSegmento = segmento({
+                px1: valoresX[0][i-1] * 100 / valoresX[1],
+                py1: -cy[i-1] * 100 / valoresY[1],
+                px2: valoresX[0][i] * 100 / valoresX[1],
+                py2: -cy[i] * 100 / valoresY[1]
+            });
+            dibSegmento.dibujarSegmento();
+        }
+    }
+    //Imprimir letra de segmento
+    //Imprimir letra de segmento > con datos obtenidos de "cy" y los valores del vector de x
+    for (let i = 0; i < numX + 1; i++) {
+        if (i == 0) {
+            const letraIni = componentesSegmento({
+                letra: letras[i],
+                px: 0,
+                py: 0
+            });
+            letraIni.dibujarLetra();
+            letraIni.dibujarPunto();
+        } else {
+            const letra = componentesSegmento({
+                letra: letras[i],
+                px: valoresX[0][i - 1] * 100 / valoresX[1],
+                py: -cy[i - 1] * 100 / valoresY[1]
+            });
+            letra.dibujarLetra();
+            letra.dibujarPunto();
+        }
+    }
+    
 });
 //FIN PROCESO >> accion del boton generar
+//INICIO PROCESO >> funcion llenar datos de ejes
+function llenarDatos(numObj) {
+    let array = [numObj], numM = 0;
+    for (let i = 0; i < numObj; i++) {
+        array[i] = Math.round(Math.random() * 100);
+        for (let j = i - 1; j >= 0; j--) { // bucle para comparar los numero ingresado con los anteriores
+            if (Math.abs(array[i] - array[j]) < 5) { // validacion por si los numeros estan menos de 5 unidaddes cerca
+                //console.log(array[i] + "->" + array[j]);
+                i--;
+            }
+        }
+        if (array[i] > numM) {
+            numM = array[i];
+        }
+    }
+    array.sort((a, b) => a - b);
+    return [array, numM];
+}
+//FIN PROCESO >> funcion llenar datos de ejes
+//INICIO PROCESO >> funcion imprimir datos de ejes
+function dibujarDatos(numObj, valores, eje) {
+    switch (eje) {
+        case "Y":
+            for (let i = 0; i < numObj; i++) {
+                const dibnumY = linNumY({
+                    porcentaje: -(valores[0][i] * 100) / valores[1],
+                    etinum: valores[0][i]
+                });
+                dibnumY.dibujarNumero();
+            }
+            break;
+        case "X":
+            for (let i = 0; i < numObj; i++) {
+                const dibnumX = linNumX({
+                    porcentaje: valores[0][i] * 100 / valores[1],
+                    etinum: valores[0][i]
+                });
+                dibnumX.dibujarNumero();
+            }
+            break;
+    }
+}
+//FIN PROCESO >> funcion imprimir datos de ejes
